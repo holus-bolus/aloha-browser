3161ae46086818c2999992f4082581b4f169506f
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-06-08 17:39:32 +0300

    Added the additional advantages page

--- app/components/AdditionalAdvantages/AdditionalAdvantages.tsx
+++ app/components/AdditionalAdvantages/AdditionalAdvantages.tsx
@@ -1,16 +1,19 @@
+import "./AdditionalAdvantages.css";
 const AdditionalAdvantages = () => {
   return (
     <section className="additional-advantages">
-      <h2 className="title">... And that's not all</h2>
+      <h2 className="title additional-advantages__title">
+        ... And that's not all
+      </h2>
       <ul className="additional-advantages-list">
         <li className="additional-advantages-item">
-          <h3 className="additional-advantages__subheading">
+          <h3 className="additional-advantages__subheading additional-advantages__subheading--light-blue">
             Browser features
           </h3>
           <ul className="additinal-advandages__inner-list">
-            <li className="additional-advantages__inner-item">Data sync</li>
+            <li className="additional-advantages__inner-item">Data sync • </li>
             <li className="additional-advantages__inner-item">
-              Aloha Premium sharing
+              Aloha Premium sharing •
             </li>
             <li className="additional-advantages__inner-item">
               Access recovery
@@ -18,21 +21,27 @@ const AdditionalAdvantages = () => {
           </ul>
         </li>
         <li className="additional-advantages-item">
-          <h3 className="additional-advantages__subheading">File manager</h3>
+          <h3 className="additional-advantages__subheading additional-advantages__subheading--light-light-blue">
+            File manager
+          </h3>
           <ul className="additinal-advandages__inner-list">
-            <li className="additional-advantages__inner-item">Manage files</li>
-            <li className="additional-advantages__inner-item">Audio & video</li>
             <li className="additional-advantages__inner-item">
-              Multi-thread downloads
+              Manage files •{" "}
             </li>
             <li className="additional-advantages__inner-item">
-              Parallel downloads
+              Audio & video •{" "}
             </li>
             <li className="additional-advantages__inner-item">
-              Private, locked folders
+              Multi-thread downloads •
             </li>
             <li className="additional-advantages__inner-item">
-              Wi-Fi file sharing
+              Parallel downloads •
+            </li>
+            <li className="additional-advantages__inner-item">
+              Private, locked folders •
+            </li>
+            <li className="additional-advantages__inner-item">
+              Wi-Fi file sharing •
             </li>
             <li className="additional-advantages__inner-item">
               Archive & unarchive with ZIP support
@@ -40,44 +49,53 @@ const AdditionalAdvantages = () => {
           </ul>
         </li>
         <li className="additional-advantages-item">
-          <h3 className="additional-advantages__subheading">Media player</h3>
+          <h3 className="additional-advantages__subheading additional-advantages__subheading--green">
+            Media player
+          </h3>
           <ul className="additinal-advandages__inner-list">
             <li className="additional-advantages__inner-item">
-              Photo slideshow
+              Photo slideshow •
             </li>
             <li className="additional-advantages__inner-item">
-              Audio playlist
+              Audio playlist •
             </li>
             <li className="additional-advantages__inner-item">
-              Background playing
+              Background playing •
+            </li>
+            <br />
+            <li className="additional-advantages__inner-item">
+              Multiple formats •
             </li>
             <li className="additional-advantages__inner-item">
-              Multiple formats
-            </li>
-            <li className="additional-advantages__inner-item">
-              Picture-in-Picture
+              Picture-in-Picture •
             </li>
             <li className="additional-advantages__inner-item">VR support</li>
             <li className="additional-advantages__inner-item">
-              Gesture controls
+              Gesture controls •
             </li>
             <li className="additional-advantages__inner-item">Chromecast</li>
           </ul>
         </li>
         <li className="additional-advantages-item">
-          <h3 className="additional-advantages__subheading">Profile</h3>
+          <h3 className="additional-advantages__subheading additional-advantages__subheading--purple">
+            Profile
+          </h3>
           <ul className="additinal-advandages__inner-list">
-            <li className="additional-advantages__inner-item">Security</li>
+            <li className="additional-advantages__inner-item">Security • </li>
             <li className="additional-advantages__inner-item">Data sync</li>
           </ul>
         </li>
         <li className="additional-advantages-item">
-          <h3 className="additional-advantages__subheading">Appearance</h3>
+          <h3 className="additional-advantages__subheading additional-advantages__subheading--orange">
+            Appearance
+          </h3>
           <ul className="additinal-advandages__inner-list">
-            <li className="additional-advantages__inner-item">Dark mode</li>
-            <li className="additional-advantages__inner-item">Color themes</li>
-            <li className="additional-advantages__inner-item">News</li>
-            <li className="additional-advantages__inner-item">Wallpapers</li>
+            <li className="additional-advantages__inner-item">Dark mode • </li>
+            <li className="additional-advantages__inner-item">
+              Color themes •
+            </li>
+            <li className="additional-advantages__inner-item">News •</li>
+            <li className="additional-advantages__inner-item">Wallpapers •</li>
           </ul>
         </li>
       </ul>
