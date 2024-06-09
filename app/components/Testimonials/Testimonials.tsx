ee4a9e33a45bbc5abfc65025a8125528389ef0a3
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-06-09 13:18:48 +0300

    Adjusted the list

--- app/components/Testimonials/Testimonials.tsx
+++ app/components/Testimonials/Testimonials.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "./Testimonials.css";
 
 const Testimonials = () => {
   return (
@@ -8,7 +9,7 @@ const Testimonials = () => {
         people in 80 countries
       </h2>
       <div className="testimonials__wrapper">
-        <div className="testimonials__apple-store">
+        <div className="testimonials__reiviews-wrapper testimonials__apple-store">
           <div className="testimonials__apple-store-badge">
             <div className="testimonials__apple-number-wrapper">
               <Image
@@ -67,7 +68,7 @@ const Testimonials = () => {
             <li className="testimonials__item"></li>
           </ul>
         </div>
-        <div className="testimonials__google-play">
+        <div className="testimonials__reiviews-wrapper testimonials__google-play">
           <div className="testimonials__playmarket-number-wrapper">
             <Image
               src="/images/testimonials/testi2.svg"
@@ -78,50 +79,50 @@ const Testimonials = () => {
             <p className="testimonials__average">4.4</p>
           </div>
           <button className="testimonials__review-button">183K reviews</button>
+          <ul className="testimonials__list">
+            <li className="testimonials__item">
+              <div className="testimonials__item-points-wrapper">
+                <img
+                  src="/images/testimonials/testimonials-star-rating.svg"
+                  alt="The star rating"
+                />
+              </div>
+              <p className="testimonials__item-text">
+                This app is amazing, definitely the best of its kind I have
+                used. Works really well for downloading and storing files. Would
+                highly recommend.
+                <span className="testimonnials__item-name"> Gavhec</span>
+              </p>
+            </li>
+            <li className="testimonials__item">
+              <div className="testimonials__item-points-wrapper">
+                <img
+                  src="/images/testimonials/testimonials-star-rating.svg"
+                  alt="The star rating"
+                />
+              </div>
+              <p className="testimonials__item-text">
+                The best app for browse everything and download.
+                <span className="testimonnials__item-name"> Roxyyyy14</span>
+              </p>
+            </li>
+            <li className="testimonials__item">
+              <div className="testimonials__item-points-wrapper">
+                <img
+                  src="/images/testimonials/testimonials-star-rating.svg"
+                  alt="The star rating"
+                />
+              </div>
+              <p className="testimonials__item-text">
+                I’m obsessed with Aloha! Great browser, real privacy, easy to
+                navigate, user friendly and now Crypto wallet?! Keep up the good
+                work!
+                <span className="testimonnials__item-name"> ToySoldier23</span>
+              </p>
+            </li>
+            <li className="testimonials__item"></li>
+          </ul>
         </div>
-        <ul className="testimonials__list">
-          <li className="testimonials__item">
-            <div className="testimonials__item-points-wrapper">
-              <img
-                src="/images/testimonials/testimonials-star-rating.svg"
-                alt="The star rating"
-              />
-            </div>
-            <p className="testimonials__item-text">
-              This app is amazing, definitely the best of its kind I have used.
-              Works really well for downloading and storing files. Would highly
-              recommend.
-              <span className="testimonnials__item-name"> Gavhec</span>
-            </p>
-          </li>
-          <li className="testimonials__item">
-            <div className="testimonials__item-points-wrapper">
-              <img
-                src="/images/testimonials/testimonials-star-rating.svg"
-                alt="The star rating"
-              />
-            </div>
-            <p className="testimonials__item-text">
-              The best app for browse everything and download.
-              <span className="testimonnials__item-name"> Roxyyyy14</span>
-            </p>
-          </li>
-          <li className="testimonials__item">
-            <div className="testimonials__item-points-wrapper">
-              <img
-                src="/images/testimonials/testimonials-star-rating.svg"
-                alt="The star rating"
-              />
-            </div>
-            <p className="testimonials__item-text">
-              I’m obsessed with Aloha! Great browser, real privacy, easy to
-              navigate, user friendly and now Crypto wallet?! Keep up the good
-              work!
-              <span className="testimonnials__item-name"> ToySoldier23</span>
-            </p>
-          </li>
-          <li className="testimonials__item"></li>
-        </ul>
       </div>
     </section>
   );
