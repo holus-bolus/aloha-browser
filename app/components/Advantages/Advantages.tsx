5c3627c8776b0de05396d0f5813b14a5567f777e
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-06-10 10:43:45 +0300

    Added the tablet big on privacy section responsiveness

--- app/components/Advantages/Advantages.tsx
+++ app/components/Advantages/Advantages.tsx
@@ -10,24 +10,30 @@ const Advantages = () => {
             src="/images/say-hello/hide-activity.png"
             alt="The image of the mobile phone with the opened browser"
             width="610"
-            loading="lazy"
             height="775"
+            loading="lazy"
           />
           <div className="advantages__text-wrapper">
             <h2 className="subheading advantages__title">
               Hide your activity
               <br />
-              from nosy people with
+              from nosy people
               <br />
-              the free VPN
+              with the free VPN
             </h2>
             <button className="button advantages__button">See more</button>
             <ul className="advantages-list">
-              <li className="list-item">Protect your online privacy</li>
-              <li className="list-item">Stay safe on public Wi‐Fi</li>
-              <li className="list-item">Get around geo‐blocking</li>
-              <li className="list-item">Browse like you're at home</li>
-              <li className="list-item">Save on costs</li>
+              <li className="advantages__list-item">
+                Protect your online privacy
+              </li>
+              <li className="advantages__list-item">
+                Stay safe on public Wi‐Fi
+              </li>
+              <li className="advantages__list-item">Get around geo‐blocking</li>
+              <li className="advantages__list-item">
+                Browse like you're at home
+              </li>
+              <li className="advantages__list-item">Save on costs</li>
             </ul>
           </div>
         </div>
@@ -44,19 +50,25 @@ const Advantages = () => {
             </h2>
             <button className="button advantages__button">See more</button>
             <ul className="advantages-list">
-              <li className="list-item">Block annoying pop‐ups & banners</li>
-              <li className="list-item">Avoid unskippable video ads</li>
-              <li className="list-item">Load pages faster</li>
-              <li className="list-item">Maximize battery life</li>
-              <li className="list-item">Keep your data & device safe</li>
+              <li className="advantages__list-item">
+                Block annoying pop‐ups & banners
+              </li>
+              <li className="advantages__list-item">
+                Avoid unskippable video ads
+              </li>
+              <li className="advantages__list-item">Load pages faster</li>
+              <li className="advantages__list-item">Maximize battery life</li>
+              <li className="advantages__list-item">
+                Keep your data & device safe
+              </li>
             </ul>
           </div>
           <Image
             src="/images/say-hello/avoid-pain.png"
             alt="The image of the mobile phone with the opened browser"
             width="700"
-            loading="lazy"
             height="870"
+            loading="lazy"
           />
         </div>
       </section>
@@ -80,10 +92,12 @@ const Advantages = () => {
             </h2>
             <button className="button advantages__button">See more</button>
             <ul className="advantages-list">
-              <li className="list-item">Buy, send & receive crypto</li>
-              <li className="list-item">Swap over 300 tokens</li>
-              <li className="list-item">Explore Web3 apps</li>
-              <li className="list-item">Store all your NFTs</li>
+              <li className="advantages__list-item">
+                Buy, send & receive crypto
+              </li>
+              <li className="advantages__list-item">Swap over 300 tokens</li>
+              <li className="advantages__list-item">Explore Web3 apps</li>
+              <li className="advantages__list-item">Store all your NFTs</li>
             </ul>
           </div>
         </div>
